c2695f63ec05cb1663c87d8ebb097a003ea20245
hanifyudhaa <hanifyudhaa@gmail.com>
2020-04-28 13:23:16 +0700

    benerin route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main');
-});
+Route::get('/',"OnlineClassController@index")->name("list");
 
 Route::resource("online-class","OnlineClassController");
